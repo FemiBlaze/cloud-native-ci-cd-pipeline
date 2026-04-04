daac241d7f48270b8090445bc28601fa062be098
Femi Blaze <blaze2dtop@gmail.com>
2026-04-04 14:10:27 +0100

    Modified Infra

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {   
     description = "The AWS region to deploy resources in"
     type        = string
-    default     = "us-east-1"
+    default     = "eu-west-1"
 }
 variable "app_name" {
     description = "The name of the application to deploy"
